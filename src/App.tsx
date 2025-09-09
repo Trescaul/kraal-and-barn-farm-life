7092c4f3f20ce0598e4eaac52cb4613ef264604b
Trescaul24 <fndungu376@gmail.com>
2025-09-09 16:36:51 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ const App = () => (
           <Route path="/fish" element={<Fish />} />
           <Route path="/goats" element={<Goats />} />
           <Route path="/vegetables" element={<Vegetables />} />
+          <Route path="/pigs" element={<pigs />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
